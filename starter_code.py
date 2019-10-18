b539fdb2f13507f9932b7913743e8865c6362653
m <miguel@miglmj.com>
2019-10-18 15:02:17 -0400

    Implement blocking using mock db operations

--- starter_code.py
+++ starter_code.py
@@ -2,6 +2,11 @@ import mock_db
 import uuid
 from worker import worker_main
 from threading import Thread
+from time import sleep, time
+
+LOCK_SENTINEL_KEY = "WRITING_BLOCKED"
+RETRY = False
+
 
 def lock_is_free():
     """
@@ -9,9 +14,28 @@ def lock_is_free():
 
         Return whether the lock is free
     """
+    return db.find_one({"_id": LOCK_SENTINEL_KEY}) is None
 
-    return True
 
+def set_lock(id):
+    """
+        Try attaining a lock to write to the database
+        Lock is "attained" by succesfully writing this value to the db
+        Inserting throws an exception if the key is already present,
+        so taking the lock fails
+    """
+    try:
+        db.insert_one({"_id": LOCK_SENTINEL_KEY, "locked": True})
+        return True
+    except Exception:
+        return False
+
+def release_lock(id):
+    """
+        Release lock by deleting the record in db, so other writes with the same
+        key to the db will be succesful.
+    """
+    db.delete_one({"_id": LOCK_SENTINEL_KEY})
 
 def attempt_run_worker(worker_hash, give_up_after, db, retry_interval):
     """
@@ -27,8 +51,24 @@ def attempt_run_worker(worker_hash, give_up_after, db, retry_interval):
                             until the lock is free, unless we have been trying for more
                             than give_up_after seconds
     """
-    if lock_is_free():
-        worker_main(worker_hash, db)
+
+    start_time = time()
+    done = False
+    while not done:
+        giveup = time() - start_time > give_up_after
+        if giveup:
+            # Could do something to keep track of timeouts here
+            break
+        if lock_is_free() and set_lock(worker_hash):
+            try:
+                worker_main(worker_hash, db)
+                done = True
+            except Exception:
+                done = not RETRY
+            finally:
+                release_lock(worker_hash)
+        else:
+            sleep(retry_interval)
 
 
 if __name__ == "__main__":
